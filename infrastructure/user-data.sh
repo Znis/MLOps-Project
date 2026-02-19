12d08de794e834ccc4221730abdec4904b7991b9
shrijanabudhathoki <shrijanabudhathoki51@gmail.com>
2026-02-19 13:30:51 +0545

    add infra code

--- infrastructure/user-data.sh
+++ infrastructure/user-data.sh
@@ -5,14 +5,14 @@ yum update -y
 yum install -y docker git
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ec2-user
-
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+usermod -aG docker ubuntu
 
 mkdir -p /usr/lib/docker/cli-plugins
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/lib/docker/cli-plugins/docker-compose
+
 curl -L https://github.com/docker/buildx/releases/download/v0.17.1/buildx-v0.17.1.linux-amd64 -o /usr/lib/docker/cli-plugins/docker-buildx
 chmod +x /usr/lib/docker/cli-plugins/docker-buildx
 
-mkdir -p /home/ec2-user/app
-chown -R ec2-user:ec2-user /home/ec2-user/app
+mkdir -p /home/ubuntu/MLOps-Project
+chown -R ubuntu:ubuntu /home/ubuntu/MLOps-Project
